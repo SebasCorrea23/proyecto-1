ad17efef81affa589dac51ecc3375176e65d371d
SENA <juansebastiancorreagarcia@gmail.com>
2024-08-08 07:08:24 -0500

    añadi amor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-// let inicio = 10
-// let final = 1
-//let incremento = 1
-
-// for (let i = inicio; i >= final ; i-=incremento) {
-    //if(i == 5) continue
-    //console.log(i);
-
-//}
-
-let i = 2 
-
-for (let i = 2; i < 6; i+=2) {
-console.log(i);
-
-}
-    
+let i = 4
